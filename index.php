b2f20f94a605a6a80574450e8130f1690570856e
James <james.gaffe78@gmail.com>
2020-02-04 12:13:07 +0100

    nouvelle index

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 include('_config.php');
-
-MyAutoload::start();
+session_start();
+require 'vendor/autoload.php';
+use Acme\Classes\Routeur;
+//MyAutoload::start();
 // url :    index.php?action=homepage    - index.php?action=contact     index.php
 
-(isset($_GET['action'])) ? $action = $_GET['action'] : $action = "homepage";
-
+(isset($_SERVER['PATH_INFO'])) ? $action = $_SERVER['PATH_INFO'] : $action = "/homepage";
 // role de routeur
 $routeur = new Routeur($action);
 $routeur->renderController();
